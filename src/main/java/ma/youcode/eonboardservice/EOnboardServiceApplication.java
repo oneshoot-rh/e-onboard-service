0a2f744d2f75b49cbcf8720be34a77a99f0892d6
mounir.elbakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-05 13:05:42 +0100

    init project

---
+++ src/main/java/ma/youcode/eonboardservice/EOnboardServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.eonboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EOnboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EOnboardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/youcode/eonboardservice/EOnboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.eonboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EOnboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

88b44c13af3550c9e8c715cd4e27d22abfaf6079
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-29 05:22:17 +0000

    expose onbrd plan api

--- src/main/java/ma/youcode/eonboardservice/onboarding/OnboardingPlanResource.java
+++ src/main/java/ma/youcode/eonboardservice/onboarding/OnboardingPlanResource.java
@@ -10,7 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/onboarding-plans")
+@RequestMapping("/v1/onboarding-plans")
 @RequiredArgsConstructor
 public class OnboardingPlanResource {
     private final OnboardingPlanService onboardingPlanService;

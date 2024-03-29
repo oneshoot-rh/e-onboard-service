7547239622d4d8d47658b91125125f4da1f6f137
mounirelbakkali1 <mounir.elbakkali1@etu.uae.ac.ma>
2024-03-29 05:28:03 +0000

    new kafka listener (tenant db setup listener)

--- src/main/java/ma/youcode/eonboardservice/EOnboardServiceApplication.java
+++ src/main/java/ma/youcode/eonboardservice/EOnboardServiceApplication.java
@@ -8,20 +8,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
 
 import lombok.RequiredArgsConstructor;
 import ma.youcode.eonboardservice.candidates.Candidate;
 import ma.youcode.eonboardservice.dossiers.Dossier;
 import ma.youcode.eonboardservice.dossiers.DossierService;
+import ma.youcode.eonboardservice.tenant.Tenant;
+import ma.youcode.eonboardservice.tenant.TenantService;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class EOnboardServiceApplication {
 
 	private final DossierService dossierService;
+	private final TenantService tenantService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EOnboardServiceApplication.class, args);
+	}
+
+	@KafkaListener(topics = "tenant-db-init", groupId = "group_id")
+	public void listenToDBInitTopic(String tenantId) {
+		System.out.println("Received Message in group group_id: " + tenantId);
+		tenantService.onBoardNewTenant(tenantId);
 	}
 
 	//@Bean
